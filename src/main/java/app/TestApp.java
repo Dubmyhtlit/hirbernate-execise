38632306214b8ab5c085a73351f174d6ab960115
Dubmythlit <arcadian424@gmail.com>
2023-08-06 09:06:31 +0800

    hibernate test

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -1,8 +1,16 @@
 package app;
 
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 
 import core.util.HibernateUtil;
 import web.member.pojo.Member;
@@ -10,17 +18,37 @@ import web.member.pojo.Member;
 public class TestApp {
 	
 	public static void main(String[] args) {
-		TestApp app = new TestApp();
-//		Member member = new Member();
-//		member.setUsername("使用者名稱");
-//		member.setPassword("密碼");
-//		member.setNickname("暱稱");
-//		member.setId(1);
-//		member.setPass(false);
-//		app.insert(member);
-//		System.out.println(member.getId());
-//		System.out.println(app.deleteById(3));
-		System.out.println(app.selectById(2).getNickname());		
+		SessionFactory sessionFactory =  HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		
+		CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+		CriteriaQuery<Member> criteriaQuery = criteriaBuilder.createQuery(Member.class);
+		Root<Member> root = criteriaQuery.from(Member.class);
+		
+		criteriaQuery.where(criteriaBuilder.and(
+				criteriaBuilder.equal(root.get("username"), "admin"),
+				criteriaBuilder.equal(root.get("password"), "p@ssw0rd")
+				));
+		
+		criteriaQuery.multiselect(root.get("username"), root.get("nickname"));
+		
+		criteriaQuery.orderBy(criteriaBuilder.asc(root.get("id")));
+		
+		Member member = session.createQuery(criteriaQuery).uniqueResult();
+		System.out.println(member.getNickname());
+		
+//		TestApp app = new TestApp();
+////		Member member = new Member();
+////		member.setUsername("使用者名稱");
+////		member.setPassword("密碼");
+////		member.setNickname("暱稱");
+////		member.setId(1);
+////		member.setPass(false);
+////		app.insert(member);
+////		System.out.println(member.getId());
+////		System.out.println(app.deleteById(3));
+////		System.out.println(app.selectById(2).getNickname());		
+//		app.selectAll().forEach(member -> System.out.println(member.getNickname()));
 	}
 	
 	public Integer insert(Member member) {
@@ -96,6 +124,21 @@ public class TestApp {
 		} 
 	}
 	
-	
+	public List<Member> selectAll() {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Query<Member> query= session.createQuery(
+					"SELECT new web.member.pojo.Member(username, nickname) FROM Member", Member.class);
+			List<Member> list = query.getResultList();
+			transaction.commit();
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		} 
+	}
 	
 }
